bee8095d5ba8586f98a9865a841d6dcc458d404e
aassandro <v.assandro@vinpearlland.com>
2020-02-24 23:02:05 +0100

    get info only for sequence active

--- python/tk_premiere/session_info.py
+++ python/tk_premiere/session_info.py
@@ -89,31 +89,34 @@ class SessionInfo(object):
 
     def __get_sequences(self, project_sequences):
         sequences = list()
+        prj = self._engine.adobe.app.project
+        active_seq = prj.activeSequence
         for s in project_sequences:
-            timebase = s.timebase
-            sequence = dict(
-                sequenceID=s.sequenceID,
-                name=s.name,
-                inPoint=s.getInPointAsTime().ticks/timebase,
-                outPoint=s.getOutPointAsTime().ticks/timebase,
-                timebase=s.timebase,
-                zeroPoint=s.zeroPoint/timebase,
-                end=s.end/timebase,
-                videoTracks=self.__get_tracks(s.videoTracks, timebase),
-                audioTracks=self.__get_tracks(s.audioTracks, timebase)
-            )
-            sequences.append(sequence)
+            if s.name == active_seq.name: 
+                timebase = s.timebase
+                sequence = dict(
+                    sequenceID=s.sequenceID,
+                    name=s.name,
+                    inPoint=s.getInPointAsTime().ticks/timebase,
+                    outPoint=s.getOutPointAsTime().ticks/timebase,
+                    timebase=s.timebase,
+                    zeroPoint=s.zeroPoint/timebase,
+                    end=s.end/timebase,
+                    videoTracks=self.__get_tracks(s.videoTracks, timebase),
+                    audioTracks=self.__get_tracks(s.audioTracks, timebase)
+                )
+                sequences.append(sequence)
         return sequences
 
     def get_info(self):
         session_info = list()
         for p in self._engine.adobe.app.projects:
-            project = dict(
-                documentID=p.documentID,
-                name=p.name,
-                path=p.path,
-                sequences=self.__get_sequences(p.sequences),
-                activeSequence=p.activeSequence
-            )
-            session_info.append(project)
+                project = dict(
+                    documentID=p.documentID,
+                    name=p.name,
+                    path=p.path,
+                    sequences=self.__get_sequences(p.sequences),
+                    activeSequence=p.activeSequence
+                )
+                session_info.append(project)
         return session_info
